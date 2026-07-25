4b8ff585536c0092efd6ef36d616779c9bc2ea68
Mian Muhammad Hassan <mmh427726@gmail.com>
2026-07-25 15:36:18 +0500

    Stage 1: database read endpoints

--- app.py
+++ app.py
@@ -1,20 +1,45 @@
 from flask import Flask, jsonify
+import sqlite3
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-        return jsonify(
-            {
-        "status": "success",
-        "message": "Welcome to my Tiny Backend API"
-            })
+def get_db_connection():
+    connection = sqlite3.connect("tasks.db")
+    connection.row_factory = sqlite3.Row
+    return connection
 
-@app.route("/hello")
-def hello():
+@app.route("/tasks", methods=["GET"])
+def get_tasks():
+    connection = get_db_connection()
+
+    tasks = connection.execute(
+        "SELECT * FROM tasks"
+    ).fetchall()
+
+    connection.close()
+
+    return jsonify([dict(task) for task in tasks])
+
+
+@app.route("/tasks/<int:task_id>", methods=["GET"])
+def get_task(task_id):
+    connection = get_db_connection()
+
+    task = connection.execute(
+        "SELECT * FROM tasks WHERE id = ?",
+        (task_id,)
+    ).fetchone()
+
+    connection.close()
+
+    if task is None:
         return jsonify({
-        "message": "Hello, World!"
-    })
+            "error": "Task not found"
+        }), 404
+
+    return jsonify(dict(task))
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
